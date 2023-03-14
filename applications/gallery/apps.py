84c35ae19a410b00818fb1365a2fd308b6436391
treflina <treflina@wp.pl>
2023-03-14 08:29:35 +0100

    images loading lazy

--- applications/gallery/apps.py
+++ applications/gallery/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from wagtail.images.apps import WagtailImagesAppConfig
 
 
 class GalleryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "applications.gallery"
+
+
+class GalleryImagesAppConfig(WagtailImagesAppConfig):
+    default_attrs = {"decoding": "async", "loading": "lazy"}
