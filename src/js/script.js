496cb6e7d8ed64be529f9ebf99ed9a38bd7a3140
treflina <treflina@wp.pl>
2023-03-29 09:45:52 +0200

    nav fixs, margins in contact

--- src/js/script.js
+++ src/js/script.js
@@ -21,13 +21,6 @@ window.onload = function () {
 
     // navbar
 
-    const navDropdownBtn = document.querySelector(".nav__item-link--collapsible");
-    function handleDropdown() {
-        document.querySelector(".nav__dropdown").classList.toggle("show");
-    }
-
-    navDropdownBtn.addEventListener("click", handleDropdown);
-
     const navMobile = document.querySelector(".nav-links");
     const navBtn = document.querySelector(".hamburger");
     const logoBtn = document.querySelector(".nav__logo");
