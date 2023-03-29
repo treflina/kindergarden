137b86af1e0d7090a159396c77caa3ca3493cd96
treflina <treflina@wp.pl>
2023-03-29 11:05:32 +0200

    map center coord

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,9 @@
 function initMap() {
     const turawa = { lat: 50.74109105338817, lng: 18.07653108465577 };
-    const turawaCenter = { lat: 50.739435904958725, lng: 18.075503040289156 };
+    const center = { lat: 50.740228564644724, lng: 18.075126082618077 };
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: turawaCenter,
+        zoom: 16,
+        center: center,
     });
     const marker = new google.maps.Marker({
         position: turawa,
