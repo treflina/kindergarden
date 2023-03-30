95f416e16d2ab189168936c536dc8d9e8d96832b
treflina <treflina@wp.pl>
2023-03-30 09:19:37 +0200

    accordion aria labels fix, smooth opening

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -32,57 +32,64 @@ accordionBtns.forEach((button) => {
         if (button.classList.contains("expanded")) {
             button.setAttribute("aria-expanded", true);
             accordionSection.setAttribute("aria-hidden", false);
+            accordionSection.style.maxHeight = `calc(${accordionSection.scrollHeight}px + ${accordionSection.parentElement.scrollHeight}px)`;
         } else {
             button.setAttribute("aria-expanded", false);
             accordionSection.setAttribute("aria-hidden", true);
+            accordionSection.style.maxHeight = 0;
         }
     });
 });
-
-const detectMob = () => {
-     return window.innerWidth <= 800 && window.innerHeight <= 1000;
- }
 
 const openFirstNews = () => {
     const firstNews = document.getElementById("accordion-section-1");
     const firstNewsBtn = document.getElementById("accordion-open-1");
     firstNews.classList.add("active");
+    firstNewsBtn.classList.add("expanded");
     firstNews.setAttribute("aria-hidden", false);
-    firstNewsBtn.setAttribute("aria-expanded", false);
+    firstNewsBtn.setAttribute("aria-expanded", true);
+    firstNews.style.maxHeight = `calc(${firstNews.scrollHeight}px + ${firstNews.parentElement.scrollHeight}px)`;
 };
 
-if (!detectMob()) {
+if (!(window.innerWidth <= 800 && window.innerHeight <= 1000)) {
     openFirstNews();
 }
 
+// const closeAccordionItem = () => {
+//     const allActiveItems = document.querySelectorAll(".active");
+//     allActiveItems.forEach((item) => {
+//         item.classList.remove("active");
+//         item.setAttribute("aria-hidden", true);
+//         item.previousElementSibling.setAttribute(
+//             "aria-expanded",
+//             false
+//         );
+//         console.log(item.previousElementSibling);
+//     });
+// };
 
-const closeAccordionItem = () => {
-    const allActiveItems = document.querySelectorAll(".news__info");
-    allActiveItems.forEach((item) => item.classList.remove("active"));
-};
+// const clickOutsideAccordion = (e) => {
+//     if (e.target.classList.contains("body")) {
+//         const i = e.target;
+//         closeAccordionItem();
+//         return;
+//     } else if (
+//         e.target.classList.contains("news__btn") ||
+//         e.target.classList.contains("news__info") ||
+//         e.target.classList.contains("news__info-text") ||
+//         e.target.classList.contains("colheaders") ||
+//         e.target.parentElement.parentElement.classList.contains("colheaders") ||
+//         e.target.parentElement.parentElement.classList.contains("tb-block") ||
+//         e.target.parentElement.parentElement.classList.contains(
+//             "thead-block"
+//         ) ||
+//         e.target.parentElement.parentElement.classList.contains("tbody-block")
+//     ) {
+//         return;
+//     } else {
+//         const i = e.target;
+//         closeAccordionItem();
+//     }
+// };
 
-const clickOutsideAccordion = (e) => {
-    if (e.target.classList.contains("body")) {
-        const i = e.target;
-        closeAccordionItem();
-        return;
-    } else if (
-        e.target.classList.contains("news__btn") ||
-        e.target.classList.contains("news__info") ||
-        e.target.classList.contains("news__info-text") ||
-        e.target.classList.contains("colheaders") ||
-        e.target.parentElement.parentElement.classList.contains("colheaders") ||
-        e.target.parentElement.parentElement.classList.contains("tb-block") ||
-        e.target.parentElement.parentElement.classList.contains(
-            "thead-block"
-        ) ||
-        e.target.parentElement.parentElement.classList.contains("tbody-block")
-    ) {
-        return;
-    } else {
-        const i = e.target;
-        closeAccordionItem();
-    }
-};
-
-window.addEventListener("click", clickOutsideAccordion);
+// window.addEventListener("click", clickOutsideAccordion);
