25ed97199cdb3106e2dabf676cd5ff4f98509b06
treflina <treflina@wp.pl>
2023-05-14 17:04:00 +0200

    first news open on mobiles

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -62,10 +62,11 @@ const openFirstNews = () => {
     firstNews.style.maxHeight = `calc(${firstNews.scrollHeight}px + ${firstNews.parentElement.scrollHeight}px)`;
 };
 
-if (!(window.innerWidth <= 800 && window.innerHeight <= 1000)
+// if (!(window.innerWidth <= 800 && window.innerHeight <= 1000)
     // && !(getCookie('firstNews'))
-) {
+// ) {
     // document.cookie =
     //     "firstNews=seen; expires=session; path=/;";
-    openFirstNews();
-}
+//     openFirstNews();
+// }
+openFirstNews();
