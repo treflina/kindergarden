c79e340dfcfcaa404832730f4156460390be0be4
treflina <treflina@wp.pl>
2023-05-28 17:41:49 +0200

    navbar accessibility enhanced

--- src/js/script.js
+++ src/js/script.js
@@ -21,53 +21,98 @@ window.onload = function () {
 
     // navbar
 
-    const navMobile = document.querySelector(".nav-links");
-    const navBtn = document.querySelector(".hamburger");
+    const dropdownBtn = document.querySelectorAll(".nav-btn");
+    const dropdown = document.querySelectorAll(".nav__dropdown");
+    const hamburgerBtn = document.querySelector(".hamburger");
     const navTxt = document.querySelector(".hamburger-text");
-    const logoBtn = document.querySelector(".nav__logo");
-    const allNavItems = document.querySelectorAll(".menu-close");
+    const navMenu = document.querySelector(".nav-links");
+    const links = document.querySelectorAll("nav__dropdown-link");
+    const allTargetNavItems = document.querySelectorAll(".menu-close");
 
-    const showDropdown = function (event) {
-        const targ = event.target;
-        const drp = document.getElementsByClassName("nav__dropdown");
-        for (let i = 0; i < drp.length; i++) {
-            if (
-                drp[i].previousElementSibling === targ ||
-                drp[i].previousElementSibling.children[0] === targ
-            ) {
-                drp[i].classList.toggle("show");
-                drp[i].previousElementSibling.classList.toggle("active");
-            } else {
-                drp[i].classList.remove("show");
-                drp[i].previousElementSibling.classList.remove("active");
-            }
-        }
-    };
+    function setAriaExpandedFalse() {
+        dropdownBtn.forEach((btn) =>
+            btn.setAttribute("aria-expanded", "false")
+        );
+    }
 
-    window.addEventListener("click", showDropdown);
+    function closeDropdownMenu() {
+        dropdown.forEach((drop) => {
+            drop.classList.remove("show");
+            drop.addEventListener("click", (e) => e.stopPropagation());
+        });
+    }
 
-    const closeNav = () => {
-        navBtn.classList.remove("is-active");
-        navMobile.classList.remove("nav-links--active");
-    };
-
-    allNavItems.forEach((item) => {
-        item.addEventListener("click", closeNav);
-    });
-
-    logoBtn.addEventListener("click", closeNav);
-
-    const handleNav = () => {
-        navBtn.classList.toggle("is-active");
-        navMobile.classList.toggle("nav-links--active");
-        if (navBtn.classList.contains("is-active")) {
+    function handleHamburger() {
+        if (hamburgerBtn.classList.contains("is-active")) {
             navTxt.textContent = "Zamknij";
+            hamburgerBtn.setAttribute("aria-expanded", "true");
         } else {
             navTxt.textContent = "Menu";
+            hamburgerBtn.setAttribute("aria-expanded", "false");
         }
+    }
+
+    function toggleHamburger() {
+        navMenu.classList.toggle("nav-links--active");
+        hamburgerBtn.classList.toggle("is-active");
+        handleHamburger();
+    }
+
+    const closeNav = () => {
+        hamburgerBtn.classList.remove("is-active");
+        navMenu.classList.remove("nav-links--active");
+        handleHamburger();
     };
 
-    navBtn.addEventListener("click", handleNav);
+    dropdownBtn.forEach((btn) => {
+        btn.addEventListener("click", function (e) {
+            const dropdownIndex = e.currentTarget.dataset.dropdown;
+            const dropdownElement = document.getElementById(dropdownIndex);
+
+            dropdownElement.classList.toggle("show");
+            dropdown.forEach((drop) => {
+                if (drop.id !== btn.dataset["dropdown"]) {
+                    drop.classList.remove("show");
+                }
+            });
+            e.stopPropagation();
+            btn.setAttribute(
+                "aria-expanded",
+                btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+            );
+        });
+    });
+
+    // close dropdown menu when the dropdown links are clicked
+    links.forEach((link) =>
+        link.addEventListener("click", () => {
+            closeDropdownMenu();
+            setAriaExpandedFalse();
+            toggleHamburger();
+        })
+    );
+
+    // close dropdown menu when you click on the document body
+    document.documentElement.addEventListener("click", () => {
+        closeDropdownMenu();
+        setAriaExpandedFalse();
+    });
+
+    // close dropdown when the escape key is pressed
+    document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            closeDropdownMenu();
+            setAriaExpandedFalse();
+        }
+    });
+
+    // toggle hamburger menu
+    hamburgerBtn.addEventListener("click", toggleHamburger);
+
+    //hide navigation when link clicked 
+    allTargetNavItems.forEach((item) => {
+        item.addEventListener("click", closeNav);
+    });
 
     //gallery
     const galleryItems = document.querySelectorAll(".gallery__item");
@@ -79,6 +124,4 @@ window.onload = function () {
     };
 
     galleryAddClass();
-
-
 };
