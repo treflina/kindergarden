e7e54b9ff016b6a5990e5e2186403717f60eed13
treflina <treflina@wp.pl>
2023-09-03 18:44:33 +0200

    replace charblock to textblock in schedulepage

--- applications/aboutus/blocks.py
+++ applications/aboutus/blocks.py
@@ -3,8 +3,9 @@ from wagtail import blocks
 class ScheduleBlock(blocks.StructBlock):
     start_hour = blocks.TimeBlock(label="Od", required=True)
     end_hour = blocks.TimeBlock(label="Do", required=True)
-    description = blocks.CharBlock(label="Opis", required=True)
+    description = blocks.TextBlock(label="Opis", required=True)
 
     class Meta: # noqa
         label = "Punkt planu dnia"
         icon = "edit"
+
