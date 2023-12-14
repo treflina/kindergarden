7120d85d7c139d7384373e80c673a1ee3e57a769
treflina <treflina@wp.pl>
2023-12-14 10:10:52 +0100

    get cookie fn enabled

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -41,16 +41,16 @@ accordionBtns.forEach((button) => {
     });
 });
 
-// function getCookie(name) {
-//     var nameEQ = name + "=";
-//     var ca = document.cookie.split(";");
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == " ") c = c.substring(1, c.length);
-//         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-//     }
-//     return null;
-// }
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
 const openFirstNews = () => {
     const firstNews = document.getElementById("accordion-section-1");
